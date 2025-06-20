d307266284bdb464f46f32b05b09840505ec6187
mcison21 <mcison21@gmail.com>
2025-06-20 15:06:18 -0400

    fix: move app_root to .env

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,21 +9,28 @@ import Index from '@/pages/Index.vue'
 import Kiosk from '@/pages/Kiosk.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
+const appRoot = import.meta.env.VITE_APP_ROOT
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/:date(\\d{4}-\\d{2}-\\d{2})?', name: 'home', component: Index, props: true },
-    { path: '/kiosk/:date(\\d{4}-\\d{2}-\\d{2})?', name: 'kiosk', component: Kiosk, props: true },
+    { path: appRoot + ':date(\\d{4}-\\d{2}-\\d{2})?', name: 'home', component: Index, props: true },
     {
-      path: '/config',
+      path: appRoot + 'kiosk/:date(\\d{4}-\\d{2}-\\d{2})?',
+      name: 'kiosk',
+      component: Kiosk,
+      props: true,
+    },
+    {
+      path: appRoot + '/config',
       name: 'config',
       component: ConfigIndex,
       redirect: { name: 'edit-menus' },
       children: [
-        { path: '/config/menus', name: 'edit-menus', component: EditMenus },
-        { path: '/config/device', name: 'edit-device', component: EditDevice },
-        { path: '/config/auth', name: 'edit-auth', component: EditAuth },
-        { path: '/config/api', name: 'edit-api', component: EditApi },
+        { path: appRoot + 'config/menus', name: 'edit-menus', component: EditMenus },
+        { path: appRoot + 'config/device', name: 'edit-device', component: EditDevice },
+        { path: appRoot + 'config/auth', name: 'edit-auth', component: EditAuth },
+        { path: appRoot + 'config/api', name: 'edit-api', component: EditApi },
       ],
     },
     {
--- vite.config.ts
+++ vite.config.ts
@@ -1,88 +1,89 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, resolve, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss(),
-    // vueDevTools(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      strategies: 'injectManifest',
-      srcDir: 'src',
-      filename: 'sw.js',
-      manifest: {
-        name: 'Dining Devive Menu Viewer',
-        short_name: 'Menu Viewer',
-        description: 'This application will be used to display menu data for the dining hall.',
-        theme_color: '#003865',
-        background_color: '#ffffff',
-        display: 'fullscreen',
-        id: '/',
-        scope: '/',
-        start_url: '/',
-        icons: [
-          {
-            src: '/dining-device-front-end/icons/apple-touch-icon-57x57.png',
-            sizes: '57x57',
-            type: 'image/png',
-          },
-          {
-            src: '/dining-device-front-end/icons/apple-touch-icon-60x60.png',
-            sizes: '60x60',
-            type: 'image/png',
-          },
-          {
-            src: '/dining-device-front-end/icons/apple-touch-icon-72x72.png',
-            sizes: '72x72',
-            type: 'image/png',
-          },
-          {
-            src: '/dining-device-front-end/icons/apple-touch-icon-76x76.png',
-            sizes: '76x76',
-            type: 'image/png',
-          },
-          {
-            src: '/dining-device-front-end/icons/apple-touch-icon-114x114.png',
-            sizes: '114x114',
-            type: 'image/png',
-          },
-          {
-            src: '/dining-device-front-end/icons/apple-touch-icon-120x120.png',
-            sizes: '120x120',
-            type: 'image/png',
-          },
-          {
-            src: '/dining-device-front-end/icons/apple-touch-icon-144x144.png',
-            sizes: '144x144',
-            type: 'image/png',
-          },
-          {
-            src: '/dining-device-front-end/icons/apple-touch-icon-152x152.png',
-            sizes: '152x152',
-            type: 'image/png',
-          },
-        ],
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const appRoot = env.VITE_APP_ROOT
+
+  return {
+    plugins: [
+      vue(),
+      tailwindcss(),
+      // vueDevTools(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        strategies: 'injectManifest',
+        srcDir: 'src',
+        filename: 'sw.js',
+        manifest: {
+          name: 'Dining Devive Menu Viewer',
+          short_name: 'Menu Viewer',
+          description: 'This application will be used to display menu data for the dining hall.',
+          theme_color: '#003865',
+          background_color: '#ffffff',
+          display: 'fullscreen',
+          scope: '/',
+          start_url: '/',
+          icons: [
+            {
+              src: appRoot + 'icons/apple-touch-icon-57x57.png',
+              sizes: '57x57',
+              type: 'image/png',
+            },
+            {
+              src: appRoot + 'icons/apple-touch-icon-60x60.png',
+              sizes: '60x60',
+              type: 'image/png',
+            },
+            {
+              src: appRoot + 'icons/apple-touch-icon-72x72.png',
+              sizes: '72x72',
+              type: 'image/png',
+            },
+            {
+              src: appRoot + 'icons/apple-touch-icon-76x76.png',
+              sizes: '76x76',
+              type: 'image/png',
+            },
+            {
+              src: appRoot + 'icons/apple-touch-icon-114x114.png',
+              sizes: '114x114',
+              type: 'image/png',
+            },
+            {
+              src: appRoot + 'icons/apple-touch-icon-120x120.png',
+              sizes: '120x120',
+              type: 'image/png',
+            },
+            {
+              src: appRoot + 'icons/apple-touch-icon-144x144.png',
+              sizes: '144x144',
+              type: 'image/png',
+            },
+            {
+              src: appRoot + 'icons/apple-touch-icon-152x152.png',
+              sizes: '152x152',
+              type: 'image/png',
+            },
+          ],
+        },
+        devOptions: {
+          enabled: false,
+          type: 'module',
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
-      devOptions: {
-        enabled: false,
-        type: 'module',
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  },
-  base:
-    process.env.NODE_ENV === 'production'
-      ? '/dining-device-front-end/' // Replace with your repo name
-      : '/',
+    base: process.env.NODE_ENV === 'production' ? '/dining-device-front-end/' : '/',
+  }
 })
