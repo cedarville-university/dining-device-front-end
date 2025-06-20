be557b1fc5bc4f6e118cac90759e21229a3035a4
mcison21 <mcison21@gmail.com>
2025-06-19 23:27:07 -0400

    feat: add base path

--- vite.config.ts
+++ vite.config.ts
@@ -81,4 +81,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  base:
+    process.env.NODE_ENV === 'production'
+      ? '/dining-device-front-end/' // Replace with your repo name
+      : '/',
 })
