1b81c1f40ac9038559bb4c5d18ad2e0e2b616d8a
mcison21 <mcison21@users.noreply.github.com>
2025-06-22 00:55:52 +0000

    deploy: 23bed5c7fe993a14d4e2686868f5926fdcb8dcf9

---
+++ registerPeriodicSync.js
@@ -0,0 +1,42 @@
+import { Temporal } from 'temporal-polyfill'
+import * as Pioneer from '@/functions/pioneerMenu'
+
+function registerPeriodicFetchPioneerMenu() {
+  console.log('checking if serviceWorker is ready')
+  navigator.serviceWorker.ready
+    ?.then((registration) => {
+      console.log(registration)
+      registration.periodicSync
+        .register('fetch-pioneer-menu', {
+          minInterval: 6 * 60 * 60 * 1000, // every 6 hours
+        })
+        .then(() => {
+          console.log('Periodic Sync registered')
+        })
+    })
+    .catch(console.error)
+}
+
+navigator.permissions
+  .query({
+    name: 'periodic-background-sync',
+  })
+  .then((status) => {
+    if (status.state === 'granted') {
+      console.log('granted')
+      registerPeriodicFetchPioneerMenu()
+    } else {
+      console.log('not granted')
+    }
+  })
+
+self.addEventListener('periodicsync', (event) => {
+  if (event.tag === 'fetch-pioneer-menu') {
+    console.clear()
+    const date = Temporal.Now.plainDateISO()
+    console.log(`fetching data for ${date.toString()}`)
+    event.waitUntil(Pioneer.fetchAndCache(date.toString()))
+    console.log(`fetching data for ${date.add({ days: 1 }).toString()}`)
+    event.waitUntil(Pioneer.fetchAndCache(date.add({ days: 1 }).toString()))
+  }
+})
