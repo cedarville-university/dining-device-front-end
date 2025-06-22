d2ad61a614f69ead5b5788b510ac5741451c938a
mcison21 <mcison21@gmail.com>
2025-06-22 14:56:51 -0400

    wip

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,11 @@ import { router } from './router'
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/dining-device-front-end/registerPeriodicSync.js', {
+      scope: '/dining-device-front-end/api',
+    })
+  })
+}
