af1a30ceb80c90cf01e5ad70eb3b730c8540a0a6
mcison21 <mcison21@gmail.com>
2025-06-21 21:12:27 -0400

    fix: using importScripts

--- public/registerPeriodicSync.js
+++ public/registerPeriodicSync.js
@@ -1,5 +1,4 @@
-import { Temporal } from 'temporal-polyfill'
-import * as Pioneer from '@/functions/pioneerMenu'
+importScripts('./functions/pioneer.ts')
 
 function registerPeriodicFetchPioneerMenu() {
   console.log('checking if serviceWorker is ready')
@@ -33,10 +32,25 @@ navigator.permissions
 self.addEventListener('periodicsync', (event) => {
   if (event.tag === 'fetch-pioneer-menu') {
     console.clear()
-    const date = Temporal.Now.plainDateISO()
-    console.log(`fetching data for ${date.toString()}`)
-    event.waitUntil(Pioneer.fetchAndCache(date.toString()))
-    console.log(`fetching data for ${date.add({ days: 1 }).toString()}`)
-    event.waitUntil(Pioneer.fetchAndCache(date.add({ days: 1 }).toString()))
+    const today = new Date()
+    const year = today.getFullYear()
+    const month = String(today.getMonth() + 1).padStart(2, '0') // Months are 0-indexed
+    const day = String(today.getDate()).padStart(2, '0')
+
+    const formattedToday = `${year}-${month}-${day}`
+
+    const tomorrow = new Date()
+    tomorrow.setDate(tomorrow.getDate() + 1) // Increment the day by 1
+
+    const tomorrowYear = tomorrow.getFullYear()
+    const tomorrowMonth = String(tomorrow.getMonth() + 1).padStart(2, '0')
+    const tomorrowDay = String(tomorrow.getDate()).padStart(2, '0')
+
+    const formattedTomorrow = `${tomorrowYear}-${tomorrowMonth}-${tomorrowDay}`
+
+    console.log(`fetching data for ${formattedToday}`)
+    event.waitUntil(fetchAndCache(formattedToday))
+    console.log(`fetching data for ${formattedTomorrow}`)
+    event.waitUntil(fetchAndCache(formattedTomorrow))
   }
 })
