8781d09bf85c3478c0cfa536b0a0f5536cd0011b
mcison21 <mcison21@gmail.com>
2025-07-11 16:58:31 -0400

    feat: abstract teleport to header into component

---
+++ src/components/FormSubmit.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import AppButton from './AppButton.vue'
+</script>
+
+<template>
+  <Teleport to="#PageHeaderAction" defer>
+    <AppButton v-bind="$attrs" type="submit">
+      <slot>Save</slot>
+    </AppButton>
+  </Teleport>
+</template>
