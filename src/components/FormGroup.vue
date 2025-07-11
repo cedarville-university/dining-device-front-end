dc7ce140d4dcb766773261c1b9c90b90b2df34c4
mcison21 <mcison21@gmail.com>
2025-07-11 16:57:12 -0400

    feat: add formgroup component

---
+++ src/components/FormGroup.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{ title?: string }>()
+</script>
+
+<template>
+  <div class="bg-white rounded-md shadow">
+    <h3 v-if="title" class="px-4 pt-4 font-semibold text-lg row-start-1 col-span-full mb-4">
+      {{ title }}
+    </h3>
+
+    <div class="p-4 space-y-4">
+      <slot />
+    </div>
+
+    <div v-if="$slots.actions" class="flex p-4 rounded-[inherit] bg-(--gray-color)/50">
+      <slot name="actions" />
+    </div>
+  </div>
+</template>
