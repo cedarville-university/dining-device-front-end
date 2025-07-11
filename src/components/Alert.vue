8e3e2666e98c67e335d6fe5387b5923fcce823d1
mcison21 <mcison21@gmail.com>
2025-07-11 16:56:34 -0400

    feat: add error variant

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import { CheckCircleIcon } from '@heroicons/vue/20/solid'
+import { CheckCircleIcon, XCircleIcon } from '@heroicons/vue/20/solid'
 import { useTemplateRef, watch } from 'vue'
 
+export type AlertVariant = 'success' | 'error'
+
 const { variant = 'success', timeout = 5000 } = defineProps<{
-  variant?: 'success'
+  variant?: AlertVariant
   timeout?: number
 }>()
 
@@ -36,9 +38,11 @@ watch(message, (newMessage) => {
     class="hidden absolute inset-[10px_10px_auto_auto] starting:-translate-y-5 starting:opacity-0 translate-y-0 opacity-100 p-4 rounded-md shadow transition-all open:flex gap-2 items-center"
     :class="{
       'bg-green-200 border border-green-500 text-green-800': variant === 'success',
+      'bg-red-200 border border-red-500 text-red-800': variant === 'error',
     }"
   >
     <CheckCircleIcon v-if="variant === 'success'" class="size-5" />
+    <XCircleIcon v-if="variant === 'error'" class="size-5" />
     {{ message }}
   </dialog>
 </template>
