2873f02820e0217908c4072e1516d2a3986d0a88
mcison21 <mcison21@gmail.com>
2025-07-11 16:48:10 -0400

    fix: prevents weird scrolling when the page is bigger than viewport

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import DeviceLayout from './layouts/DeviceLayout.vue'
 </script>
 
 <template>
-  <div class="group/main @container/main min-h-dvh grid place-content-center">
+  <div class="group/main @container/main">
     <DeviceLayout>
       <RouterView />
     </DeviceLayout>
