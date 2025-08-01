0310d9efc10251e90b28c798d09d6f4a6a1c78c6
mcison21 <mcison21@gmail.com>
2025-08-01 12:12:14 -0400

    fix: card updates to work with main dish

--- src/components/Allergen.vue
+++ src/components/Allergen.vue
@@ -5,14 +5,12 @@ defineProps<{ allergen: string }>()
 </script>
 
 <template>
-  <figure
-    class="flex flex-col items-center justify-between overflow-clip rounded-md shadow bg-(--canvas-bg) w-full"
-  >
+  <figure class="flex flex-col items-center justify-between rounded-md bg-(--canvas-bg)/25 w-full">
     <img
       :src="allergen"
       class="w-5/6 @[200px]/allergens:w-2/3 @[350px]/allergens:w-1/2 grid my-2 bg-white rounded-full border-2 border-white shadow"
     />
-    <figcaption class="bg-white w-full p-1 text-xs max-w-full">
+    <figcaption class="w-full p-1 text-xs max-w-full">
       {{ allergenLabels.get(allergen) }}
     </figcaption>
   </figure>
