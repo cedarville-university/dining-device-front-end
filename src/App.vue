2c3c92c6eb2f131033c549b5a0602f77a5fd2f68
mcison21 <mcison21@gmail.com>
2025-08-08 13:17:19 -0400

    test: dropped min-h-dvh
    trying to get the devices to not overscroll

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import DeviceLayout from './layouts/DeviceLayout.vue'
 </script>
 
 <template>
-  <div class="group/main @container/main min-h-dvh">
+  <div class="group/main @container/main">
     <DeviceLayout class="mx-auto my-auto">
       <RouterView />
     </DeviceLayout>
